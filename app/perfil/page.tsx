7c41c699ef26f2456802396d9ff02a5412360c26
jsa2021 <83621711+jsa2021@users.noreply.github.com>
2024-02-26 21:59:00 -0300

    Add files via upload

--- app/perfil/page.tsx
+++ app/perfil/page.tsx
@@ -15,36 +15,34 @@ export default function PerfilPage() {
 
 
 
-  // Get the value from local storage if it exists
+  // // Get the value from local storage if it exists
   // value1 = localStorage.getItem("Nombre") || "";
   // value2 = localStorage.getItem("Peso") || "";
   // value3 = localStorage.getItem("Altura") || "";
 
 
 
-  // Set the value received from the local storage to a local state
-  const [Nombre, setNombre] = useState(value1);
-  const [Peso, setPeso] = useState(value2);
-  const [Altura, setAltura] = useState(value3);
+  // // Set the value received from the local storage to a local state
+  // const [Nombre, setNombre] = useState(value1);
+  // const [Peso, setPeso] = useState(value2);
+  // const [Altura, setAltura] = useState(value3);
 
-  // When user submits the form, save the data to local storage
-  const saveToLocalStorage = (e: { preventDefault: () => void; }) => {
-    e.preventDefault();
-    localStorage.setItem("Nombre", Nombre);
-    localStorage.setItem("Peso", Peso);
-    localStorage.setItem("Altura", Altura);
-  };
+  // // When user submits the form, save the data to local storage
+  // const saveToLocalStorage = (e: { preventDefault: () => void; }) => {
+  //   e.preventDefault();
+  //   localStorage.setItem("Nombre", Nombre);
+  //   localStorage.setItem("Peso", Peso);
+  //   localStorage.setItem("Altura", Altura);
+  // };
 
   return (
     <div>
       <label htmlFor="number">Your favorite number</label>
       <Divider className="my-4" />
       <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-        <form onSubmit={saveToLocalStorage}>
+        <form>
           <Input
             id="nombre"
-            value={Nombre}
-            onChange={(e) => setNombre(e.target.value)}
             type=""
             label="Nombre"
             placeholder="Ingresa tu nombre"
@@ -52,8 +50,6 @@ export default function PerfilPage() {
           <Divider className="my-4" />
           <Input
             id="altura"
-            value={Altura}
-            onChange={(e) => setAltura(e.target.value)}
             type=""
             label="Altura"
             placeholder="Ingresa tu altura"
@@ -61,8 +57,6 @@ export default function PerfilPage() {
           <Divider className="my-4" />
           <Input
             id="peso"
-            value={Peso}
-            onChange={(e) => setPeso(e.target.value)}
             type=""
             label="Peso"
             placeholder="Ingresa tu peso"
